d9e3dd4647fcd368f04361c129d349e9c7c93154
Drew Wanczowski <awanczow@gmail.com>
2026-07-30 17:09:10 -0400

    Update for vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Replace 'strategy-engine' with your EXACT GitHub repository name
   base: '/financial-strategy-engine/', 
 })
